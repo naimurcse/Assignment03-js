83ce58381332049fedb423c751bf201dc651e837
Naimur Rahaman <2015naim@gmail.com>
2021-01-22 15:50:53 +0600

    hotelCost works

--- assignment.js
+++ assignment.js
@@ -20,7 +20,34 @@ function budgetCalculator(noOfWatch, noOfMobile, noOfLeptop){
     return budget;
 }
 
-var totalAmount = budgetCalculator(3,5,2);
+var totalAmount = budgetCalculator(8,3,5);
 console.log(totalAmount);
+
+
+// Calculation of Hotel Cost
+function hotelCost(noOfDay){
+    var roomRate=0;
+    if(noOfDay<=10){
+        roomRate = noOfDay * 100;
+        return roomRate;
+    }else if(noOfDay<=20){
+        var first10daysCost = 10 * 100;
+        var remainingDay = noOfDay - 10;
+        var remainingDayCost = remainingDay * 80; 
+        roomRate = first10daysCost + remainingDayCost;
+        return roomRate;
+    }else{
+        first10daysCost = 10 * 100;
+        second10daysCost = 10 * 80;
+        remainingDay = noOfDay - 20;
+        remainingDayCost = remainingDay * 50;
+        roomRate = first10daysCost + second10daysCost + remainingDayCost;
+        return roomRate;
+    }
+}
+
+var totaHotelCost = hotelCost(22);
+console.log(totaHotelCost);
+
 
 
